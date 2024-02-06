26ff37e0377bcd1684e9227c68c8ba334c72cec4
HarrySmith81224 <107133689+HarrySmith81224@users.noreply.github.com>
2024-02-06 00:19:19 +0000

    commit1

---
+++ awol1/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    redirects(){
+        return [
+            {
+            source: "/",
+            destination: "/discover/now_playing",
+            permanent: true,
+            },
+        ];
+    },
+
+
+};
+
+export default nextConfig;
